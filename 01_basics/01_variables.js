352751c8287794f40879bf6747af81367bca78d2
AlwaysSeverusSnape <142542521+AlwaysSeverusSnape@users.noreply.github.com>
2023-08-17 22:00:18 +0000

    L4(variables)

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144823
+let accountEmail = "bfdffdfdnndjnn@gmail.com"
+var accountPassword = "1234"
+accountCity = "Ambikapur"
+let accountState
+
+//accountId = 5    //Ye const hai isko aage change nahi kar sakte error dega
+console.log(accountId)
+accountEmail = "dfdbffd@@gmail.comm"
+accountPassword = "654978"
+accountCity = "Raipur"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+prefer not use var beacuse of issue in block scope and functional scope
+*/
