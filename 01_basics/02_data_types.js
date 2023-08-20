f1d4620a47a68699616e2ce3de021a96fb49392c
AlwaysSeverusSnape <142542521+AlwaysSeverusSnape@users.noreply.github.com>
2023-08-20 21:03:42 +0000

    datatypes

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,25 @@
+"use strict";   // to use the 
+
+let name="aryan"
+let age = 21
+let is_loggedIn  = false
+let state = null
+
+//number => 2^53
+//bigint
+//string = ""
+//boolean =>true/false
+//undefined => value is to be assigned
+//symbol     => used for uniqueness
+
+
+
+//object
+
+console.log(typeof "aryan");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
