f9c0653c8c0f0125c7860075f0e078463409d855
AlwaysSeverusSnape <142542521+AlwaysSeverusSnape@users.noreply.github.com>
2023-08-20 23:48:33 +0000

    Memory concept

---
+++ 01_basics/06_memory_theory_interviews.js
@@ -0,0 +1,6 @@
+// There are two types of memories
+//Stack(Primitive data type k liye)  and Heap(Non-Primitive data type k liye)
+
+// link of video  https://www.youtube.com/watch?v=7gwc-1czolw&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=10
+
+
