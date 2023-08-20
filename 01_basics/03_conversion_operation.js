ce3c2402769327930fc3c26a20e19b75bdc6e3b7
AlwaysSeverusSnape <142542521+AlwaysSeverusSnape@users.noreply.github.com>
2023-08-20 21:51:24 +0000

    conversion_operation

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -1,7 +1,19 @@
-let score="33"
+let score="33abc"
 
 console.log(typeof score);
 console.log(typeof(score));  //both are same
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber); 
+console.log(valueInNumber);
+//33abc is not a number therefore output comes NaN => Not A Number
+
+//"33" => number
+//"33abs" => NaN but its type is Number, when we wrote the vale than NaN output comes
+//true => 1 and false => 0
+
+
+let is_LoggedIn =1
+
+let booleanis_LoggedIn = Boolean (is_LoggedIn)
+console.log(booleanis_LoggedIIn);
