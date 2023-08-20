3982480214eb97741ca4f0f022802d4d24c2a250
AlwaysSeverusSnape <142542521+AlwaysSeverusSnape@users.noreply.github.com>
2023-08-20 23:32:55 +0000

    datatypes Interviews

---
+++ 01_basics/05_data_types_for_interview.js
@@ -0,0 +1,24 @@
+//just go and watch Hitesh choudhary hindi lecture number 9 
+// link =>   https://www.youtube.com/watch?v=suMvZWjjKbo&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=9
+
+//link for Documentation => https://262.ecma-international.org/5.1/#sec-11.4.3  // Iska Table to Yaad Rehna Hi Chahiye 
+
+
+/* Learn this table
+
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+
+*/
+
